804c7df17d6a3bce37f646fface4164f8b00525a
Akshay Telore <teloreakshay1000@gmail.com>
2025-02-22 10:51:52 +0530

    Initial commit - Set up project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
-    </div>
-  );
+    <h1 className="text-3xl font-bold underline">
+      Hello world hi!
+    </h1>
+  )
 }
