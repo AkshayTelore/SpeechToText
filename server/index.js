02bd3954ab566bba7a01e15f3a1a2f0bd9d55ffc
Akshay Telore <teloreakshay1000@gmail.com>
2025-03-04 10:04:19 +0530

    Cleared repository

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const cors = require("cors");
-const { default: mongoose } = require("mongoose");
-const app = express();
-app.use(cors());
-app.use(express.json());
-mongoose.connect("mongodb://localhost:27017/userSpeechRecord");
-app.listen(5000, () => console.log("Server running on port 5000"));
